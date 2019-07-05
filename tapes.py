6a0d614758165bf2b09919b8aae87fa6ec31f752
Mitchell Kuiper <mitchell.kuiper13@gmail.com>
2019-07-05 12:37:49 -0700

    Fixes and comment edits

--- tapes.py
+++ tapes.py
@@ -42,7 +42,7 @@ def print_menu2(title, choices, prompt='Choose an option'):
     Args:
         title (str): Title to menu
         choices (list): Strings of all the available choices
-        prompt (str): Prompt for to help user with menu
+        prompt (str): Prompt to help user with menu
     """
     buffer = int((70 - len(title))/2) # Buffer for styling menu
     print(buffer * '-' , title , buffer * '-')
@@ -72,21 +72,36 @@ def print_menu2(title, choices, prompt='Choose an option'):
 
     
 def get_info():
+    """Helper function used to get tape info from user
+
+    Returns: tape_type, tape_shape, tape_count, direction
+        tape_type (int): Index pointing to the tape associated with tapes list
+        tape_shape (str): The name of the shape User chose 
+        tape_count (int): Number of sides for shape (1-7)
+        direction (str): String of the direction of triangle; square if square
+    """
+
+    # Request tape type from user
     choices = ['Cassettes', 'VHS', 'Floppy', 'SNES']
-    # Convert menu choice index to tapes index
+    # Convert menu choice option to tapes index
     tape_type = print_menu2('Tape Type', choices, 'Choose a type of tape to be printed.') - 1
 
+    # Request tape shape from user
     choices = ['Square', 'Triangle']
+    # Convert menu choice to shape name
     tape_shape = choices[print_menu2('Shape', choices, 'Choose a shape for the ' + tapes[tape_type][0])-1]
 
+    # Request direction from user if triangle
     # User chose Triangle
     if tape_shape == 'Triangle':
         choices = ['Top Left', 'Top Right', 'Bottom Left', 'Bottom Right']
-        direction = print_menu2('Triangle Position', choices, 'Choose a direction the Triangle faces.')
-    else: 
-        direction = 0 # Default the direction
-    
-    choices = ['1', '2', '3', '4', '5', '6', '7']
+        # Convert menu choice to shape
+        direction = choices[print_menu2('Triangle Position', choices, 'Choose a direction the Triangle faces.')-1]
+    else:
+        direction = 'Square'
+
+    # Request number of sides for shape
+    choices = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven']
     tape_count = print_menu2('Side length', choices, 'Choose the side length of the ' + tape_shape)
     
     return tape_type, tape_shape, tape_count, direction
@@ -96,37 +111,34 @@ def print_tapes(tape_type, tape_shape, tape_length, direction):
     """Prints out the tapes with given args
 
     Args:
-        tape_type (str): Type of tape to be printed out
+        tape_type (int): Index pointing to tape type in tapes list
         tape_length (int): Length of the side of square or right triangle
-        tape_shape (int): 1 = Square, 2 = Triangle
+        tape_shape (str): Name of the shape to be printed out
+        direction (str): Direction to print the triange, otherwise 'square'
     """
     # Print square shape
-    if tape_shape == 'Square' or direction == 0:
-        for i in range(0, tape_length):
+    if tape_shape == 'Square' or direction == 'Square':
+        for row_count in range(0, tape_length):
             for line_chunk in tapes[tape_type][1]:
-                for j in range(0, tape_length):
-                    print(" " + line_chunk + " ", end="")
-                print('\r')
+                print((" " + line_chunk + " ")*tape_length)
     else: 
-        if direction == 1:
+        if direction == 'Top Left':
             # Print triangle in top left
-            col_count = tape_length
             for row_count in range(0, tape_length):
                 for line_chunk in tapes[tape_type][1]:
-                    print((" " + line_chunk + " ") * col_count)
-                col_count-=1
-        elif direction == 2:
+                    print((" " + line_chunk + " ") * (tape_length - row_count))
+        elif direction == 'Top Right':
             # Print triangle in top right
             for row_count in range(0, tape_length):
                 for line_chunk in tapes[tape_type][1]:
                     print(' '*(len(line_chunk)+2)*row_count, end='')
                     print((' ' + line_chunk + ' ') * (tape_length - row_count))
-        elif direction == 3:
+        elif direction == 'Bottom Left':
             # Print triangle in bottom left
             for row_count in range(0, tape_length):
                 for line_chunk in tapes[tape_type][1]:
                     print((' ' + line_chunk + ' ') * (row_count + 1))
-        elif direction == 4:
+        elif direction == 'Bottom Right':
             # Print triangle in bottom right
             for row_count in range(0, tape_length):
                 for line_chunk in tapes[tape_type][1]:
@@ -134,10 +146,9 @@ def print_tapes(tape_type, tape_shape, tape_length, direction):
                     print((" " + line_chunk + " ") * (row_count + 1))
         else:
             print('Problem with triangle direction.')
-        
-    
 
 while True:
+    
     tape_type, tape_shape, tape_count, direction = get_info()
     print_tapes(tape_type, tape_shape, tape_count, direction)
 
